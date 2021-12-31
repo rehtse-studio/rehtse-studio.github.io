d46dd154a4cace11e45c5a9e9e5c2ce6fb7c3ffd
Rehtse Studio <efelicies@letsgomango.com>
2021-12-31 09:13:17 -0400

    Minor changes

--- package/scripts.js
+++ package/scripts.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var w = window.location.href;
 
-    if (w == "https://rehtse-studio.github.io/links") {
+    if (w == "https://rehtse-studio.github.io/pages/links") {
         window.history.replaceState("https://rehtse-studio.github.io/", "links", "/links");
     }
 }
